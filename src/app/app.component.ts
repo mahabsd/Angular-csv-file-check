8dda5ce81b46e2999f1786b80f45e9fa8435a629
masmoudi-mehdi <massmoudi.mehdi95@gmail.com>
2021-04-14 15:04:47 +0100

    add main and mapping component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from "@angular/core";
 import { FileService } from "./services/fileUpload.service";
+import { MappingServices } from "./services/mapping.service";
 
 @Component({
   selector: "app-root",
@@ -7,93 +8,5 @@ import { FileService } from "./services/fileUpload.service";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  @ViewChild("fileDropRef", { static: false }) fileDropEl: ElementRef;
-  files: any[] = [];
-  file: File;
-  formData: any;
-
-  constructor(private fileService : FileService){
-
-  }
-  /**
-   * on file drop handler
-   */
-  onFileDropped($event) {
-    console.log($event.target.files[0]);
-    this.prepareFilesList($event);
-  }
-
-  /**
-   * handle file from browsing
-   */
-  fileBrowseHandler(event) {
-    this.formData = new FormData();
-    if (event.target.value) {
-      this.file = <File>event.target.files[0];
-      this.formData.append('file', this.file, this.file.name);
-    }
-    this.fileService.addFile(this.formData).subscribe(res => console.log(res))
-    this.prepareFilesList(event.target.files);
-  }
-
-  /**
-   * Delete file from files list
-   * @param index (File index)
-   */
-  deleteFile(index: number) {
-    if (this.files[index].progress < 100) {
-      console.log("Upload in progress.");
-      return;
-    }
-    this.files.splice(index, 1);
-  }
-
-  /**
-   * Simulate the upload process
-   */
-  uploadFilesSimulator(index: number) {
-    setTimeout(() => {
-      if (index === this.files.length) {
-        return;
-      } else {
-        const progressInterval = setInterval(() => {
-          if (this.files[index].progress === 100) {
-            clearInterval(progressInterval);
-            this.uploadFilesSimulator(index + 1);
-          } else {
-            this.files[index].progress += 5;
-          }
-        }, 200);
-      }
-    }, 1000);
-  }
-
-  /**
-   * Convert Files list to normal array list
-   * @param files (Files List)
-   */
-  prepareFilesList(files: Array<any>) {
-    for (const item of files) {
-      item.progress = 0;
-      this.files.push(item);
-    }
-    this.fileDropEl.nativeElement.value = "";
-    this.uploadFilesSimulator(0);
-  }
-
-  /**
-   * format bytes
-   * @param bytes (File size in bytes)
-   * @param decimals (Decimals point)
-   */
-  formatBytes(bytes, decimals = 2) {
-    if (bytes === 0) {
-      return "0 Bytes";
-    }
-    const k = 1024;
-    const dm = decimals <= 0 ? 0 : decimals;
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-  }
+ constructor(){}
 }
