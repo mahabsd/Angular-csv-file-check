53f5b429affb6066ee46c95d7483fee6fa5e3a25
sofienkarka <sofien.gallas@gmail.com>
2021-04-19 14:04:05 +0200

    fuzzy version0

--- src/app/mapping/mapping.component.ts
+++ src/app/mapping/mapping.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { MappingServices } from '../services/mapping.service';
+import FuzzySet from 'fuzzyset.js'
 
 @Component({
   selector: 'app-mapping',
@@ -7,30 +8,64 @@ import { MappingServices } from '../services/mapping.service';
   styleUrls: ['./mapping.component.scss']
 })
 export class MappingComponent implements OnInit {
-  founded: any[];
+  founded: string[];
   notFounded: any[];
   possibleChoices :any[];
   confirmedFounded = ["yess1", "yess2", "yess3"];
-  bla
-
+  Reference=["FirstName","LastName","Language","PayId","PayId1"]
+  maxMatch:any[]=[];
+  maxMatchWord:string[]=[];
   constructor(private mappingService : MappingServices) {
   }
 
   ngOnInit() {
     this.founded = this.mappingService.foundedValue;
-    console.log(this.founded);
     this.notFounded = this.mappingService.notFoundedValue;
     this.possibleChoices = this.mappingService.reqValue;
-    console.log("aaaaaaaaaaaaaaaaaaaaaaaaa ");
-    console.log(this.founded[0][1]);
-    console.log("aaaaaaaaaaaaaaaaaaaaaaaaa " );
 
     for (let i = 0; i < this.founded[0].length; i++) {
       console.log("am working on this founded i this.founded " + this.founded[0][i]);
       this.possibleChoices = this.mappingService.arrayRemove(this.possibleChoices, this.founded[0][i])
       console.log("and the possible choices are know " + this.possibleChoices);
     }
+    this.matchingWord()
   }
+matchingWord(){
+let i=0;
+var valeur=0;
+var word:string='';
+const fuzzyArray=[]
+  const matchingWords=FuzzySet(this.Reference,false)
+ 
+    const long=this.founded[0].length;
+    
+   this.founded.forEach(element=>{
+     for(i=0;i<long;i++)  {
+    const matchingTab =matchingWords.get(element[i]) 
+    fuzzyArray.push(matchingTab)
+   
+  }
+  console.log(fuzzyArray);
+  
+    fuzzyArray.forEach(aray=>{
+      valeur=0;
 
+   aray.forEach(element => {
+    if(element[0]>valeur){
+      valeur=element[0]
+      word=element[1]
+      this.maxMatch.push(valeur)
+      this.maxMatchWord.push(word)
+    }
+
+  });
+  })
+   console.log(this.maxMatch);
+   console.log(this.maxMatchWord);
+
+   })
+
+   
+}
 
 }
