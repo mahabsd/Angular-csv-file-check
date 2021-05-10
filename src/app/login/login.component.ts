50c79cbef8b3565a44e9ab93527dca0720c70128
Mahabsd <Mahabsd@gmail.com>
2021-05-10 13:19:21 +0100

    login validation email and password and delete console.log in all files

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,13 +13,14 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   registerForm: FormGroup;
   show: boolean = false;
-
+  hide: boolean = false;
+  submitted: boolean = false;
   constructor(public loginService: LoginService, private router: Router) { }
 
   ngOnInit() {
     this.loginForm = new FormGroup({
-      email: new FormControl('',  [Validators.required]),
-      password: new FormControl('',  [Validators.required])
+      email: new FormControl('', [Validators.required, , Validators.email]),
+      password: new FormControl('', [Validators.required])
     })
     this.registerForm = new FormGroup({
       email: new FormControl(''),
@@ -31,7 +32,13 @@ export class LoginComponent implements OnInit {
     this.loginService.addUser(form).subscribe()
   }
   submitLogin(form) {
-    this.loginService.loginUser(form).subscribe((res:{message: string; token: string})=>{
+    this.submitted = true;
+    if (form.invalid) {
+      this.hide = true
+      return;
+    }
+
+    this.loginService.loginUser(form).subscribe((res: { message: string; token: string }) => {
       localStorage.setItem('token', res.token);
       this.router.navigate(['home-expensya']);
       this.loginService.show = true;
